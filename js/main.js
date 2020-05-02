8402d54a345ff485d5e01d922ca134a902daaac7
nanacorobi-77 <naakyun7@gmail.com>
2020-05-02 10:56:55 +0900

    確率の操作を追加

--- js/main.js
+++ js/main.js
@@ -5,9 +5,19 @@
 {
   const btn = document.getElementById("btn");
   btn.addEventListener("click", () => {
-    const results = ["大吉", "中吉", "凶", "末吉"];
-    const n = Math.floor(Math.random() * results.length);
+    //const results = ["大吉", "中吉", "凶", "末吉"];
+    // const results = ["大吉", "大吉", "大吉", "凶"];
+    // const n = Math.floor(Math.random() * results.length);
     // btn.textContent = n;
-    btn.textContent = results[n];
+    // btn.textContent = results[n];
+
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = "大吉"; //５％
+    } else if (n < 0.02) {
+      btn.textContent = "中吉"; //15%
+    } else {
+      btn.textContent = "凶"; //80%
+    }
   });
 }
